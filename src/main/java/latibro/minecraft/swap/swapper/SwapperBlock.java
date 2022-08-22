93cdcbbf86292d8ac31cc740733832490b4d78ed
Latibro <mail@latibro.com>
2022-08-22 23:11:53 +0200

    Now both swaps BlockState and BlockEntity

--- src/main/java/latibro/minecraft/swap/swapper/SwapperBlock.java
+++ src/main/java/latibro/minecraft/swap/swapper/SwapperBlock.java
@@ -1,17 +1,30 @@
 package latibro.minecraft.swap.swapper;
 
+import latibro.minecraft.swap.SwapMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-
+import net.minecraft.world.World;
 import javax.annotation.Nullable;
+import java.util.Objects;
 
 public class SwapperBlock extends Block {
 
+    public static final BooleanProperty TRIGGERED = BlockStateProperties.TRIGGERED;
+
     public SwapperBlock() {
         super(Properties.of(Material.METAL));
+
+        this.registerDefaultState(this.defaultBlockState().setValue(TRIGGERED, false));
+        //this.registerDefaultState(this.stateDefinition.any().setValue(STATE, 0));
     }
 
     @Override
@@ -23,5 +36,46 @@ public class SwapperBlock extends Block {
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new SwapperBlockEntity();
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean x) {
+        if (!world.isClientSide) {
+            boolean previousPowered = state.getValue(TRIGGERED);
+            boolean currentPowered = world.getDirectSignalTo(pos) > 0;
+            //SwapMod.LOGGER.warn("SWAPPER " + previousPowered + " -> " + currentPowered + " " + pos + " " + fromPos + " " + state + " " + x);
+
+            if (previousPowered == currentPowered) {
+                // No powered change
+                //SwapMod.LOGGER.warn("SWAPPER no change");
+                return;
+            }
+
+            if (previousPowered) {
+                // Power off
+                //SwapMod.LOGGER.warn("SWAPPER power off");
+                world.setBlockAndUpdate(pos, state.setValue(TRIGGERED, false));
+                return;
+            }
+
+            //SwapMod.LOGGER.warn("SWAPPER power on");
+            world.setBlockAndUpdate(pos, state.setValue(TRIGGERED, true));
+
+            SwapMod.LOGGER.warn("SWAPPER swap");
+
+            ((SwapperBlockEntity) world.getBlockEntity(pos)).swap();
+        }
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> state) {
+        state.add(TRIGGERED);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        //TODO set state based on neigbor redstone
+        return super.getStateForPlacement(context);
     }
 }
--- src/main/java/latibro/minecraft/swap/swapper/SwapperBlockEntity.java
+++ src/main/java/latibro/minecraft/swap/swapper/SwapperBlockEntity.java
@@ -1,11 +1,69 @@
 package latibro.minecraft.swap.swapper;
 
+import latibro.minecraft.swap.SwapMod;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
 
 import static latibro.minecraft.swap.SwapMod.SWAPPER_BLOCK_ENTITY;
 
 public class SwapperBlockEntity extends TileEntity {
+
+    //TODO store storedBlockState in NBT
+    private BlockState storedBlockState = Blocks.AIR.defaultBlockState();
+    //TODO store storedNBT in NBT
+    private CompoundNBT storedNBT = null;
+
     public SwapperBlockEntity() {
         super(SWAPPER_BLOCK_ENTITY.get());
     }
+
+    public void swap() {
+        SwapMod.LOGGER.warn("EEE swapping");
+
+        SwapMod.LOGGER.warn("EEE stored state " + storedBlockState);
+        if (storedNBT != null) {
+            SwapMod.LOGGER.warn("EEE stored NBT " + storedNBT);
+        }
+
+        BlockState currentBlockState = getLevel().getBlockState(getBlockPos().above());
+        TileEntity currentBlockEntity = getLevel().getBlockEntity(getBlockPos().above());
+        CompoundNBT currentNBT = null;
+
+        SwapMod.LOGGER.warn("EEE current state " + currentBlockState);
+        SwapMod.LOGGER.warn("EEE current entity " + currentBlockEntity);
+        if (currentBlockEntity != null) {
+            currentNBT = currentBlockEntity.serializeNBT();
+            SwapMod.LOGGER.warn("EEE current NBT " + currentNBT);
+        }
+
+        // Set new to stored
+        BlockState newBlockState = storedBlockState;
+        CompoundNBT newNBT = storedNBT;
+
+        // Set stored to current
+        storedBlockState = currentBlockState;
+        storedNBT = currentNBT;
+
+        // Clear inventory to prevent item drops when removed
+        if (currentBlockEntity instanceof IInventory) {
+            ((IInventory) currentBlockEntity).clearContent();
+        }
+
+        // Swap current for new
+        getLevel().setBlockAndUpdate(getBlockPos().above(), newBlockState);
+        SwapMod.LOGGER.warn("EEE new state " + newBlockState);
+        TileEntity newBlockEntity = getLevel().getBlockEntity(getBlockPos().above());
+        SwapMod.LOGGER.warn("EEE new entity " + newBlockEntity);
+        if (newBlockEntity != null) {
+            newBlockEntity.deserializeNBT(newNBT);
+            SwapMod.LOGGER.warn("EEE new NBT " + newNBT);
+        }
+
+        // notify neighbors
+        getLevel().updateNeighborsAt(getBlockPos().above(), getBlockState().getBlock());
+    }
+
 }
